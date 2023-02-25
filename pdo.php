663e60cf3fc31816b9ca52ed3360be8f19011480
Hu Kaixiang <maxhu787@gmail.com>
2023-02-25 17:57:59 +0800

    update new database

--- pdo.php
+++ pdo.php
@@ -2,6 +2,20 @@
 session_start();
 ob_start();
 ini_set('display_errors', 0);
+
+$HOST = 'sql.freedb.tech';
+$PORT = 3306;
+$DB_NAME = 'freedb_g4o2-chat';
+$DB_USER = 'freedb_maxhu787';
+$DB_PASSWORD = getenv('DB_PASSWORD');
+$pdo = new PDO(
+    "mysql:host=$HOST;port=$PORT;dbname=$DB_NAME",
+    $DB_USER,
+    $DB_PASSWORD
+);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
 /*
 $HOST = 'sql12.freemysqlhosting.net';
 $PORT = 3306;
@@ -15,7 +29,7 @@ $pdo = new PDO(
 );
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 */
-
+/*
 $HOST = 'localhost';
 $PORT = 3306;
 $DB_NAME = 'sql12561191';
@@ -27,3 +41,4 @@ $pdo = new PDO(
     $DB_PASSWORD
 );
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+*/
