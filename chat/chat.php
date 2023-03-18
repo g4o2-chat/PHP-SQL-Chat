38a41a7a1b0bcf984bd8d1980a362c8afa75ec61
Hu Kaixiang <maxhu787@gmail.com>
2023-03-18 22:05:37 +0800

    e

--- chat/chat.php
+++ chat/chat.php
@@ -1,6 +1,5 @@
 <?php
 require_once "../pdo.php";
-date_default_timezone_set('Asia/Taipei');
 
 if (!isset($_SESSION["email"])) {
     include 'head.php';
@@ -41,27 +40,12 @@ if (!isset($_SESSION["email"])) {
         const input = document.getElementById('message-input');
         const submitBtn = document.getElementById('submit');
         const user_id = '<?= $_SESSION['user_id'] ?>';
-        /*do {
-            username = prompt('Username');
-        } while (username.match(/[^a-zA-Z0-9_]+/g) || username == "")
-        if (username) {
-            socket.emit('user-connect', username);
-            socket.emit('load-messages', username);
-        }*/
+
         socket.emit('user-connect', user_id);
         socket.emit('load-messages', user_id);
 
-        function isScrolledToBottom() {
-            var difference = messages.scrollHeight - messages.offsetHeight - messages.scrollTop;
-            return difference <= 1;
-        }
-
         function chatScroll() {
-            /*if (messages.scrollTop >= (messages.scrollHeight - messages.offsetHeight) - 100) {
-                messages.scrollTop = messages.scrollHeight;
-            } else {
-                messages.scrollTop = messages.scrollHeight;
-            }*/
+            //not working
             messages.scrollTop = messages.scrollHeight;
         }
 
@@ -73,44 +57,47 @@ if (!isset($_SESSION["email"])) {
                 .replace(/'/g, "&#x27;")
                 .replace(/"/g, "&quot;");
         }
-        messages.addEventListener("scroll", (event) => {
-            console.log(messages.scrollTop);
-            console.log(messages.scrollHeight);
-            console.log(messages.offsetHeight);
-        });
+        //fetch messages on page load
         fetch('http://localhost:3000/db/chatlog')
             .then((response) => response.text())
             .then((body) => {
                 data = JSON.parse(body);
                 data = data['responce'];
                 for (let i = 0; i < data.length; i++) {
+                    let username;
+                    let user_id;
+                    let pfpsrc;
                     fetch(`http://localhost:3000/db/users/${data[i]['user_id']}`)
                         .then((response) => response.text())
                         .then((body) => {
                             user_json = JSON.parse(body);
-                            let pfpsrc = '../assets/images/default-user-round.png';
+                            pfpsrc = '../assets/images/default-user-round.png';
                             if (user_json['pfp']) {
                                 pfpsrc = user_json['pfp']
                             }
-                            let username = user_json['username'];
-                            let pfp = `<a class="pfp-link" href="./profile.php?id=${data[i]['user_id']}"><img class="profile-image" src="${pfpsrc}"></a>`;
-                            let user = `<a href="./profile.php?id=${data[i]['user_id']}" class="account rainbow_text_animated">${username}</a>`;
-                            let message = escapeHtml(data[i]["message"]);
-                            let msg_parent_id = data[i]['message_id'] + "parent";
-                            let stamp = data[i]["message_date"];
-                            let info = `<p class="stats">${user} (${stamp})</p>`;
-                            let editBtn = "";
-
-                            if (data[i]['user_id'] == <?= $_SESSION['user_id'] ?>) {
-                                editBtn = `<button class="btn chat-btn" onclick="handleEdit(${data[i]['message_id']})">Edit</button>`;
-                            }
-                            let msg = `<p class="msg" id="${msg_parent_id}"><span id="${data[i]['message_id']}">${message}</span> ${editBtn}</p>`;
-                            let div = `<div style="margin-left: 10px;margin-top: 18px;">${info}${msg}</div>`;
-
-                            $("#chatcontent").append(pfp);
-                            $("#chatcontent").append(div);
-                            chatScroll();
+                            username = user_json['username'];
+                            user_id = user_json['user_id'];
                         })
+                    let pfp = `<a class="pfp-link" href="./profile.php?id=${user_id}"><img class="profile-image" src="${pfpsrc}"></a>`;
+                    let user = `<a href="./profile.php?id=${user_id}" class="account rainbow_text_animated">${username}</a>`;
+                    let message = data[i]["message"];
+                    message = escapeHtml(message);
+                    let msg_parent_id = data[i]['message_id'] + "parent";
+                    let message_date = data[i]["message_date"];
+                    let localDate = new Date(message_date.toLocaleString());
+
+                    let info = `<p class="stats">${user} (${localDate})</p>`;
+                    let editBtn = "";
+
+                    if (user_id == <?= $_SESSION['user_id'] ?>) {
+                        editBtn = `<button class="btn chat-btn" onclick="handleEdit(${data[i]['message_id']})">Edit</button>`;
+                    }
+                    let msg = `<p class="msg" id="${msg_parent_id}"><span id="${data[i]['message_id']}">${message}</span> ${editBtn}</p>`;
+                    let div = `<div style="margin-left: 10px;margin-top: 18px;">${info}${msg}</div>`;
+
+                    $("#chatcontent").append(pfp);
+                    $("#chatcontent").append(div);
+                    chatScroll();
                 }
             });
 
@@ -127,26 +114,6 @@ if (!isset($_SESSION["email"])) {
         })
 
         socket.on('message-submit', function(messageDetails) {
-            /*let pfpsrc = '../assets/images/default-user-round.png';
-            let pfp = `<a class="pfp-link" href="./profile.php?id=${messageDetails['user_id']}"><img class="profile-image" src="${pfpsrc}"></a>`;
-            let user = `<a href="./profile.php?id=${messageDetails['user_id']}" class="account rainbow_text_animated">${messageDetails['account']}</a>`;
-            let message = escapeHtml(messageDetails['message']);
-            let msg_parent_id = messageDetails['message_id'] + "parent";
-            let stamp = messageDetails["message_date"];
-            let info = `<p class="stats">${user} (${stamp})</p>`;
-            let editBtn = "";
-
-            if (messageDetails['user_id'] === sessionStorage.getItem("user_id")) {
-                editBtn = `<button class="btn chat-btn" onclick="handleEdit(${messageDetails['message_id']})">Edit</button>`;
-            }
-            let msg = `<p class="msg" id="${msg_parent_id}"><span id="${messageDetails['message_id']}">${message}</span> ${editBtn}</p>`;
-            let div = `<div style="margin-left: 10px;margin-top: 18px;">${info}${msg}</div>`;
-
-            $("#chatcontent").append(pfp);
-            $("#chatcontent").append(div);
-
-            chatScroll();*/
-
             fetch(`http://localhost:3000/db/users/${messageDetails['user_id']}`)
                 .then((response) => response.text())
                 .then((body) => {
@@ -160,8 +127,10 @@ if (!isset($_SESSION["email"])) {
                     let user = `<a href="./profile.php?id=${messageDetails['user_id']}" class="account rainbow_text_animated">${username}</a>`;
                     let message = escapeHtml(messageDetails["message"]);
                     let msg_parent_id = messageDetails['message_id'] + "parent";
-                    let stamp = messageDetails["message_date"];
-                    let info = `<p class="stats">${user} (${stamp})</p>`;
+                    let message_date = messageDetails["message_date"];
+                    let localDate = new Date(message_date.toLocaleString());
+
+                    let info = `<p class="stats">${user} (${localDate})</p>`;
                     let editBtn = "";
 
                     if (messageDetails['user_id'] == <?= $_SESSION['user_id'] ?>) {
@@ -180,7 +149,7 @@ if (!isset($_SESSION["email"])) {
         form.addEventListener('submit', function(e) {
             e.preventDefault();
             if (input.value) {
-                let date = new Date().toLocaleString();
+                let date = new Date().toUTCString()
                 let message = input.value
                 messageDetails = {
                     user_id: user_id,
