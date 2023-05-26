e29a38c1827d659bfe5a3d17e0e8400141db5c1c
Hu Kaixiang <maxhu787@gmail.com>
2023-05-26 23:43:23 +0800

    bug fixes login page

--- login.php
+++ login.php
@@ -4,18 +4,14 @@ require_once "head.php";
 
 date_default_timezone_set('Asia/Taipei');
 
-$host = $_SERVER['HTTP_HOST'];
-$ruta = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-$url = "http://$host$ruta";
-
 if (isset($_POST["cancel"])) {
-    header("Location: $url/index.php");
+    header("Location: ./index.php");
     die();
 }
 
 if (isset($_COOKIE['email']) && isset($_COOKIE['password'])) {
-    unset($SESSION["username"]);
-    unset($SESSION["user_id"]);
+    unset($_SESSION["username"]);
+    unset($_SESSION["user_id"]);
     session_destroy();
     session_start();
     $salt = getenv('SALT');
@@ -36,7 +32,7 @@ if (isset($_COOKIE['email']) && isset($_COOKIE['password'])) {
         if ($row['disabled'] === "True") {
             $_SESSION["error"] = "Account disabled";
             error_log("Login fail disabled account " . $_COOKIE['email'] . " " . $ip . " (" . date(DATE_RFC2822) . ")\n", 3, "./logs/logs.log");
-            header("Location: $url/login.php");
+            header("Location: ./login.php");
             die();
         }
         if ($_COOKIE['email'] == 'g4o2@protonmail.com' || $_COOKIE['email'] == 'g4o3@protonmail.com' || $_POST["email"] == 'maxhu787@gmail.com') {
@@ -48,25 +44,25 @@ if (isset($_COOKIE['email']) && isset($_COOKIE['password'])) {
         $_SESSION["username"] = $row["username"];
         $_SESSION['email'] = $row['email'];
         $_SESSION["success"] = "Logged in.";
-        if(isset($_GET['page'])) {
-            if($_GET['page'] == 'chat') {
-                header("Location: $url/chat/chat.php");
+        if (isset($_GET['page'])) {
+            if ($_GET['page'] == 'chat') {
+                header("Location: ./chat/chat.php");
             }
         } else {
-            header("Location: $url/index.php");
+            header("Location: ./index.php");
         }
         die();
     } else {
         $_SESSION["error"] = "Incorrect email or password";
         error_log("Login fail wrong password " . $_COOKIE['email'] . " " . $check . " " . $ip . " (" . date(DATE_RFC2822) . ")\n", 3, "./logs/logs.log");
-        header("Location: $url/login.php");
+        header("Location: ./login.php");
         die();
     }
 }
 
 if (isset($_POST["email"]) && isset($_POST["pass"])) {
-    unset($SESSION["username"]);
-    unset($SESSION["user_id"]);
+    unset($_SESSION["username"]);
+    unset($_SESSION["user_id"]);
     session_destroy();
     session_start();
     $salt = getenv('SALT');
@@ -87,7 +83,7 @@ if (isset($_POST["email"]) && isset($_POST["pass"])) {
         if ($row['disabled'] === "True") {
             $_SESSION["error"] = "Account disabled";
             error_log("Login fail disabled account " . $_POST['email'] . " " . $ip . " (" . date(DATE_RFC2822) . ")\n", 3, "./logs/logs.log");
-            header("Location: $url/login.php");
+            header("Location: ./login.php");
             die();
         }
         if ($_POST["email"] == 'g4o2@protonmail.com' || $_POST["email"] == 'g4o3@protonmail.com' || $_POST["email"] == 'maxhu787@gmail.com') {
@@ -106,16 +102,16 @@ if (isset($_POST["email"]) && isset($_POST["pass"])) {
         }
         if (isset($_GET['page'])) {
             if ($_GET['page'] == 'chat') {
-                header("Location: $url/chat/chat.php");
+                header("Location: ./chat/chat.php");
             }
         } else {
-            header("Location: $url/index.php");
+            header("Location: ./index.php");
         }
         die();
     } else {
         $_SESSION["error"] = "Incorrect email or password";
         error_log("Login fail wrong password " . $_POST['email'] . " " . $check . " " . $ip . " (" . date(DATE_RFC2822) . ")\n", 3, "./logs/logs.log");
-        header("Location: $url/login.php");
+        header("Location: ./login.php");
         die();
     }
 }
@@ -185,7 +181,7 @@ if (isset($_POST["email"]) && isset($_POST["pass"])) {
 </head>
 
 <body class="text-center">
-    <form class="form-signin" method="post">
+    <form class="form-signin" method="post" onsubmit="return doValidate();">
         <img class="mb-4" src="./favicon.ico" alt="" width="72" height="72">
         <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
         <p>
@@ -200,16 +196,16 @@ if (isset($_POST["email"]) && isset($_POST["pass"])) {
             }
             ?>
         </p>
-        <label for="inputEmail" class="sr-only">Email address</label>
+        <label for="id_email" class="sr-only">Email address</label>
         <input type="email" id="id_email" class="form-control" name="email" placeholder="Email address" required="" autofocus="">
-        <label for="inputPassword" class="sr-only">Password</label>
+        <label for="id_pass" class="sr-only">Password</label>
         <input type="password" id="id_pass" class="form-control" name="pass" placeholder="Password" required="">
         <div class="checkbox mb-3">
             <label>
                 <input type="checkbox" name="remember" value="remember" checked> Remember me
             </label>
         </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit" onclick="return doValidate();">Sign in</button>
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
         <p class="mt-5 mb-3 text-muted">© <?= date("Y") ?></p>
         <p>Don't have an account yet? <a href='./signup.php'>register</a></p>
     </form>
